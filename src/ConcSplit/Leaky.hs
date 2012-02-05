e8d1f0505723c3bc754d1c97eeb06429d57fcc6b
Daniel Díaz <diaz.carrete@gmail.com>
2012-02-05 01:43:17 +0100

    "leaky" implementation complete but there is a but in it

--- src/ConcSplit/Leaky.hs
+++ src/ConcSplit/Leaky.hs
@@ -5,13 +5,49 @@ module ConcSplit.Leaky (
     ) where 
 
 import System.IO
+import qualified Data.ByteString as B
+import Control.Monad
+import Control.Monad.IO.Class
 import qualified ConcSplit as CS
+import Control.Concurrent
+import Data.List
+import qualified Data.List as L
+import qualified Data.Iteratee as I
+import Data.Iteratee.IO.Handle
+import Data.Iteratee ((><>),(<><))
 
 impl:: CS.Impl
 impl= CS.Impl ["leaky","vanilla"] concSplitFiles splitHandle 
 
 concSplitFiles:: Int -> [FilePath] -> [(FilePath,Int)] -> IO ()
-concSplitFiles _ _ _ = return ()
+concSplitFiles chunkSize filesToJoin pieces = do
+    let allFileEnum =  L.map (enumFile chunkSize) filesToJoin
+        globalEnum = L.foldl1' composeEnums allFileEnum 
+    finalIterState <- globalEnum $ splitterIter pieces
+    I.run finalIterState
+
+composeEnums e1 e2 = \i -> e1 i >>= e2
 
 splitHandle:: Int -> Handle -> [(FilePath,Int)] -> IO ()
-splitHandle _ _ _ = return ()
+splitHandle chunkSize handle pieces = do
+    finalIterState <- enumHandle chunkSize handle $ splitterIter pieces
+    I.run finalIterState
+
+splitterIter:: [(FilePath,Int)] -> I.Iteratee B.ByteString IO ()
+splitterIter [] = return ()
+splitterIter ((path,size):xs) = do
+    handle <- liftIO $ openFile path WriteMode 
+    liftIO $ putStrLn $ show size
+    liftIO $ putStrLn $ "created file " ++ path
+    liftIO $ threadDelay (1*1000^2)
+    takeNIterHandle size handle
+    liftIO $ hClose handle    
+    splitterIter xs
+
+takeNIterHandle:: Int -> Handle -> I.Iteratee B.ByteString IO ()
+takeNIterHandle n handle = I.joinI $ I.take n $ iterHandle handle
+
+iterHandle:: Handle -> I.Iteratee B.ByteString IO ()
+iterHandle handle = I.mapChunksM_ $ B.hPut handle 
+
+
